0974e7b495d9d9519018e2282c98c234c04495ad
Huanzhi Mao <huanzhimao@gmail.com>
2022-10-18 02:55:12 -0700

    update skeleton code

---
+++ src/components/User/Login/Login.tsx
@@ -0,0 +1,10 @@
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>FIXME</h1>
+    </div>
+  );
+};
+export default Login;
--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import i18n from "./utils/i18n";
-import { I18nextProvider } from "react-i18next";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
+    <App />
   </React.StrictMode>
 );
 
