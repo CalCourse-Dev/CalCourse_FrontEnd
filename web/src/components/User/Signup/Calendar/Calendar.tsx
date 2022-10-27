8fb76ffa2cce2e8cdf92b93bdefc89718f414bb4
Xiaowen Yu <yxw2002@berkeley.edu>
2022-10-27 10:24:15 -0700

    finish sign up slot view

---
+++ web/src/components/User/Signup/Calendar/Calendar.tsx
@@ -0,0 +1,146 @@
+import React, { useState, useEffect } from 'react'
+import {FiChevronLeft, FiChevronRight} from "react-icons/fi"
+import { format, addMonths, subMonths, startOfMonth, endOfMonth, isThisMonth } from "date-fns";
+import "./Calendar.css"
+
+export interface calendarProps {
+    setSelectedDate: (date: Date) => void
+}
+
+const Calendar = (props: calendarProps) => {
+
+    const [currentDate, setCurrentDate] = useState(new Date());
+    const [currentDay, setCurrentDay] = useState(0);
+    const [selectedDay, setSelectedDay] = useState(0);
+    const [firstRowDays, setFirstRowDays] = useState(0);
+    const [endDay, setEndDay] = useState(0);
+
+    const dateFormat = "MMMM yyyy";
+    const weekdays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+
+    const nextMonth = () => {
+        setCurrentDate(addMonths(currentDate, 1));
+    }
+
+    const prevMonth = () => {
+        setCurrentDate(subMonths(currentDate, 1));
+    }
+
+    const updateFirstRowDays = (monthStart: Date) => {
+        setFirstRowDays((7 - weekdays.indexOf(format(monthStart, "E").toUpperCase())) % 7)
+    }
+
+    const passSelectedDate = (day: number) => {
+        if (day == currentDay) {
+            props.setSelectedDate(currentDate)
+        } else {
+            let year = parseInt(format(currentDate, "yyyy"))
+            let month = parseInt(format(currentDate, "L"))
+            let date = new Date(year, month, day)
+            props.setSelectedDate(date)
+        }
+    }
+
+    const updateAllInfo = () => {
+        let monthStart = startOfMonth(currentDate);
+        updateFirstRowDays(monthStart);
+        let endDay = endOfMonth(monthStart);
+        setEndDay(parseInt(format(endDay, "d")));
+        setSelectedDay(0)
+        if (isThisMonth(currentDate)) {
+            setCurrentDay(parseInt(format(currentDate, "d")))
+        } else {
+            setCurrentDay(32)
+        }
+    }
+
+    useEffect(() => {
+        updateAllInfo()
+        // console.log("current date", currentDate);
+        // console.log("firstRowDays", firstRowDays);
+        // console.log("endDay", endDay)
+        // console.log("current day", currentDay)
+    }, [currentDate])
+
+  return (
+    <div className='w-auto h-auto flex flex-col'>
+        <div className='w-full flex justify-between p-2 pl-3 pr-3 mb-1 items-center'>
+            <span className='text-[#262626]'>{ format(currentDate, dateFormat) }</span>
+            <div className='flex gap-2'>
+                <button className='hover:bg-[#dfe8ff] w-[2.7rem] h-[2.7rem] rounded-full flex justify-center items-center text-[#2279ff]' onClick={prevMonth}>
+                    <FiChevronLeft size={22}/>
+                </button>
+                <button className='hover:bg-[#dfe8ff] w-[2.7rem] h-[2.7rem] rounded-full flex justify-center items-center text-[#2279ff]' onClick={nextMonth}>
+                    <FiChevronRight size={22}/>
+                </button>
+            </div>
+        </div>
+        <div className='grid grid-rows gap-1.5'>
+            <div className='grid grid-cols-7 text-sm gap-1.5'>
+                {
+                    weekdays.map((day, index) => (
+                        <button key={`weekday-${index}`} className='w-[3rem] h-[3rem] sm:w-[2.5rem] sm:h-[2.5rem]'>{day}</button>
+                    ))
+                }
+            </div>
+            {
+                firstRowDays > 0 && 
+                (
+                    <div className='grid grid-cols-7 justify-end gap-1.5 text-[#828282] font-[500]'>
+                    {
+                        [...new Array<number>(7 - firstRowDays)].map((_, day) => day + 1).map((day, index) => (
+                            <button key={`weekday-block-${index}`}
+                                    disabled = {day < currentDay}
+                                    className={`w-[3rem] h-[3rem] text-lg sm:w-[2.5rem] sm:h-[2.5rem] sm:text-md
+                                                ${day >= currentDay && "available"}
+                                                ${day == selectedDay && "selected"}`}>
+                            </button>
+                        ))
+                    }
+                    {
+                        [...new Array<number>(firstRowDays)].map((_, day) => day + 1).map((day, index) => (
+                            <button key={`weekday-${index}`}
+                                    onClick={() => { 
+                                        setSelectedDay(day)
+                                        passSelectedDate(day)
+                                    }
+                                    } 
+                                    disabled = {day < currentDay}
+                                    className={`w-[3rem] h-[3rem] text-lg sm:w-[2.5rem] sm:h-[2.5rem] sm:text-md
+                                                ${day >= currentDay && "available"}
+                                                ${day == selectedDay && "selected"}`}>
+                                        {day}
+                            </button>
+                        ))
+                    }
+                    </div> 
+                )
+            }
+            <div className='grid grid-cols-7 gap-1.5 text-[#828282] font-[500]'>
+                {
+                    [...new Array<number>(endDay - firstRowDays)].map((_, day) => day + firstRowDays + 1).map((day, index) => (
+                        <div className='relative' 
+                            onClick={() => { 
+                                setSelectedDay(day)
+                                passSelectedDate(day)
+                            }}>
+                            <button key={`weekday-in-month-${index}`} 
+                                disabled = {day < currentDay}
+                                className={`w-[3rem] h-[3rem] text-lg sm:w-[2.5rem] sm:h-[2.5rem] sm:text-md
+                                            ${day >= currentDay && day != selectedDay && "available"}
+                                            ${day == selectedDay && "selected"}`}>
+                                    {day}
+                            </button>
+                            {day == currentDay && <span className={`absolute w-full left-0 bottom-0.5 sm:bottom-0 text-center text-3xl
+                                                                    ${day != selectedDay && "text-[#0069ff]"}
+                                                                    ${day == selectedDay && "text-white"}`}>.</span>}
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Calendar
--- web/src/components/User/Signup/Signup.tsx
+++ web/src/components/User/Signup/Signup.tsx
@@ -1,8 +1,87 @@
+import React, { useState } from 'react'
+import { MdAccessTimeFilled } from "react-icons/md"
+import { GiEarthAmerica } from "react-icons/gi"
+import { format } from "date-fns";
+import Calendar from './Calendar/Calendar'
 import "./Signup.css";
+
 const Signup = () => {
+
+    const timeData = ["9:00am", "9:30am", "10:00am", "10:30am", "11:00am", "11:30am", "12:00pm", "12:30pm", "1:00pm"]
+    const [selectedDate, _setSelectedDate] = useState(null);
+    const [selectedTime, setSelectedTime] = useState("")
+    const [lastSelectedTime, setLastSelectedTime] = useState("")
+
+    const setSelectedDate = (date:any) => {
+        _setSelectedDate(date);
+        setSelectedTime("");
+        setLastSelectedTime("");
+    }
+
   return (
-    <div>
-      <h1>FIXME</h1>
+    <div className="w-[100vw] h-[100vh] lg:h-[100vh] md:h-auto bg-[#fbfcfd] flex justify-center items-start tall:items-center">
+      <div className="w-auto h-[40rem] bg-white border-[#e8e8e8] border-[1px] rounded-lg shadow-md flex lg:flex-col lg:h-auto md:flex-col md:h-auto">
+        <div className='w-[27rem] md:w-[100%] lg:w-[46rem] h-full xl:border-r-[1.2px] p-7 font-semibold flex flex-col lg:items-center lg:border-b-[1.2px] md:items-center md:border-b-[1.2px]'>
+            <span className='text-[#767676] text-lg'>Hans Mao</span>
+            <h1 className='text-3xl text-[#1d1d1d]'>30 Minute Meeting</h1>
+            <div className='flex items-center gap-2 mt-5 text-[#737373]'>
+                <MdAccessTimeFilled size={28}/>
+                <span className='text-lg'>30 min</span>
+            </div>
+        </div>
+        <div className='flex justify-center lg:w-auto md:flex-col md:items-center'>
+          <div className='w-[27rem] h-full p-7 flex flex-col items-center md:w-[100%]'>
+              <span className='text-xl font-semibold w-full mb-3'>Select a Date & Time</span>
+              <Calendar setSelectedDate={setSelectedDate}/>
+              <div className='flex flex-col w-full mt-5'>
+                  <span className='font-bold'>Time Zone</span>
+                  <div className='p-2 flex gap-3 items-center text-[#1a1a1a]'>
+                      <GiEarthAmerica />
+                      <span>Pacific Time - US & Canada</span>
+                  </div>
+              </div>
+          </div>
+          {
+              selectedDate !== null && (
+                  <div className='flex flex-col w-[19rem] md:w-[100%] pt-[4rem] h-full lg:h-[35rem] md:h-[26rem] md:pt-[0rem]'>
+                      <span className='mb-5 text-[1.05rem] md:text-[1.5rem] md:text-center sm:text-center'>{format(selectedDate, "EEEE, LLLL d")}</span>
+                      <div className='flex flex-col gap-2 overflow-y-auto overflow-x-hidden md:items-center'>
+                          {timeData.map((time, index) => selectedTime !== time ? (
+                              <div className='w-[16rem] flex justify-between'>
+                                  <button key={ `time-first-row-time-${index}`}
+                                      onClick = {() => { 
+                                          setLastSelectedTime(selectedTime);
+                                          setSelectedTime(time) 
+                                      }}
+                                      className={`w-[16rem] pt-3.5 pb-3.5 border-[1.3px] border-[#89b5ff] rounded-md flex justify-center items-center hover:border-blue-500 hover:border-[1.6px]
+                                                  ${lastSelectedTime === time && 'expand-origin-left'}`}>
+                                      <span className='text-[#0069ff] font-bold'>{time}</span>
+                                  </button>
+                                  <button key={ `time-first-row-confirm-${index}`}
+                                      className={`pt-3.5 pb-3.5 border-[1.3px] bg-[#0069ff] rounded-md flex justify-center items-center
+                                                  ${lastSelectedTime != time && 'hidden'}
+                                                  ${lastSelectedTime == time && 'w-[0rem] shrink-confirm border-none'}`}>
+                                      <span className='text-white font-bold'>Confirm</span>
+                                  </button>
+                              </div>
+                          ) : (
+                              <div className='w-[16rem] flex justify-between'>
+                                  <div key={ `time-rest-rows-time-${index}`}
+                                      className={`w-[7.85rem] pt-3.5 pb-3.5 border-[1.3px] bg-[#696969] rounded-md flex justify-center items-center shrink-origin-left`}>
+                                      <span className='text-white font-bold'>{time}</span>
+                                  </div>
+                                  <button key={ `time-rest-rows-confirm-${index}`}
+                                      className={`w-[7.85rem] pt-3.5 pb-3.5 border-[1.3px] bg-[#0069ff] rounded-md flex justify-center items-center expand-origin-right`}>
+                                      <span className='text-white font-bold'>Confirm</span>
+                                  </button>
+                              </div>
+                          ))}
+                      </div>
+                  </div>
+              )
+          }
+        </div>
+      </div>
     </div>
   );
 };
