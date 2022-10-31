873b12aba17368b319cf733de24f63efd1e9f021
Darren Wang <hdwang@berkeley.edu>
2022-10-30 22:52:02 -0700

    Dashboard Submit Partially for Debug

--- web/src/components/Dashboard/Dashboard.tsx
+++ web/src/components/Dashboard/Dashboard.tsx
@@ -1,35 +1,27 @@
 import "./Dashboard.css";
 import QRCard from "./QRCard/QRCard";
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { getAllCourses } from "../../requests/get-requests/get-all-courses";
 
 export const courseContext = createContext({});
 
 const Dashboard = () => {
 
-  var array = new Array<any>()
+  const [array, setArray] = useState([]);
 
-  const consoleHelper = (data: any) => {
-    console.log(data);
-  };
+  getAllCourses(
+    "email",
+    "password",
+    (res: any) => {
+      setArray(res["Items"].map((course: any) => {
+        return QRCard(course);
+      }))
+    },
+    (data: any) => {
+      console.log(data);
+    });
 
-  const courseHelper = (res: any) => {
-    array = res["Items"].map((course: any) => {
-      return QRCard(course);
-    })
-  };
-
-  getAllCourses("email", "password", courseHelper, consoleHelper);
-
-  const testCard = QRCard({
-    course_name: "string",
-    course_term: "string",
-    course_id: "string",
-    course_qr_code_url: "string",
-  })
-
-  // return value is supposed to be *array*
-  return <div id="main-container">{testCard}</div>;
+  return <div id="main-container">{array}</div>;
 };
 
 export default Dashboard;
