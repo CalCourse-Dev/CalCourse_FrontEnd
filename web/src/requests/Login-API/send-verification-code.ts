ccaa6602dbaf5d7aa994582ab3587f2551174127
Huanzhi Mao <huanzhimao@gmail.com>
2022-11-02 01:56:30 -0700

    minor fix

---
+++ web/src/requests/Login-API/send-verification-code.ts
@@ -0,0 +1,13 @@
+import { basePostRequest } from '../base-requests';
+
+export const sendVerificationCode = (
+  email_address: string,
+  responseHandler: (data: any) => void,
+  errorHandler: (error: any) => void
+) => {
+  basePostRequest(
+    "/email/send_verification_code/" + email_address,
+    responseHandler,
+    errorHandler
+  );
+};
---
+++ web/src/requests/Login-API/verify-email-address.ts
@@ -0,0 +1,16 @@
+import { basePostRequest } from '../base-requests';
+
+export const verifyEmailAddress = (
+  email_address: string,
+  verified_status: string,
+  user_name: string,
+  responseHandler: (data: any) => void,
+  errorHandler: (error: any) => void
+) => {
+  basePostRequest(
+    "/email/verify_google_email/" + email_address + "/" + 
+        verified_status + "/" + user_name,
+    responseHandler,
+    errorHandler
+  );
+};
