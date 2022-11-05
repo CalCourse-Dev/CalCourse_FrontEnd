19be847a79bb557c6e7bbda2298c25b92ae067a9
Darren Wang <hdwang@berkeley.edu>
2022-11-05 03:51:51 -0700

    Dashboard Upload for Debug

--- web/src/components/Dashboard/Dashboard.tsx
+++ web/src/components/Dashboard/Dashboard.tsx
@@ -2,26 +2,50 @@ import "./Dashboard.css";
 import QRCard from "./QRCard/QRCard";
 import { createContext, useState } from "react";
 import { getAllCourses } from "../../requests/get-requests/get-all-courses";
+import { Input } from 'antd';
 
 export const courseContext = createContext({});
 
 const Dashboard = () => {
 
+  var terms = new Array<string>();
+
   const [array, setArray] = useState([]);
+
+  const { Search } = Input;
 
   getAllCourses(
     "email",
     "password",
     (res: any) => {
-      setArray(res["Items"].map((course: any) => {
-        return QRCard(course);
-      }))
+      setArray(res["Items"])
     },
     (data: any) => {
       console.log(data);
     });
 
-  return <div id="main-container">{array}</div>;
+  var courseQRCode = (a: Array<any>) => {
+    return a.map((course: any) => {
+      return QRCard(course);
+    });
+  }
+
+  const onSearch = (value: string) => {
+    return <div id="main-container">{courseQRCode(array.filter((course: any) => course["course_name"].includes(value)))}</div>;
+  };
+
+  terms = Array.from(new Set(array.map((course: any) => { return course["course_term"] })));
+
+  return (
+    <courseContext.Provider value={{ allTerms: terms }}>
+      <Search
+        placeholder="Input search text"
+        enterButton="Search"
+        size="large"
+        onSearch={onSearch}
+      />
+    </courseContext.Provider>
+  );
 };
 
 export default Dashboard;
