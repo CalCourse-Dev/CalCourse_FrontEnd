670e3cccd2d9da36271879e45db46c303c886e53
Huanzhi Mao <huanzhimao@gmail.com>
2022-11-05 22:06:39 -0700

    updated inferface and included API documentation

--- web/src/utils/interfaces.ts
+++ web/src/utils/interfaces.ts
@@ -1,18 +1,18 @@
 export interface UserData {
-  email: string;
-  wechat_id: string;
+  email: string,
+  wechat_id: string,
 }
 
 export interface CourseData {
-  course_name: string;
-  course_term: string;
-  course_id: string;
-  course_qr_code_url: string;
+  course_name: string,
+  course_term: string,
+  course_id: number,
+  course_qr_code_url: string,
 }
 
 export interface MissingRecord {
-  department_code: string;
-  course_code: string;
-  lecture_id: string;
-  course_term: string;
+  department_code: string,
+  course_code: string,
+  lecture_id: string,
+  course_term: string,
 }
