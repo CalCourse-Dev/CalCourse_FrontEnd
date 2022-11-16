802dcb62f46367e1a1034585b2227cdeee424f34
Huanzhi Mao <huanzhimao@gmail.com>
2022-11-16 01:45:25 -0800

    bug fix

--- web/src/utils/context.ts
+++
@@ -1,5 +0,0 @@
-import { useState } from "react";
-
-export const [allTerms, setAllTerms] = useState<string[]>([]);
-export const [userEmail, setUserEmail] = useState<string>("");
-export const [userAccessToken, setUserAccessToken] = useState<string>("");
