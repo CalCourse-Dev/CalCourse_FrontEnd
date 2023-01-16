13637e828850b4bb478ac00bede9bf02b07c81d8
akmazian <113816245+akmazian@users.noreply.github.com>
2023-01-16 14:32:52 -0800

    Created modified version of QRCard

---
+++ web/src/components/AZ's Dashboard/QRCard/QRCard.tsx
@@ -0,0 +1,31 @@
+import "./QRCard.css";
+import { QRCodeSVG } from "qrcode.react";
+import { CourseData } from "../../../utils/interfaces";
+
+const QRCard = (props: CourseData) => {
+
+  return (
+    <div id="card-container" key={props["course_name"]}>
+      <div className="card">
+        <div className="id-wrapper">
+          <div className="id">
+            <span>
+              <span>{props["course_name"]}</span>
+            </span>
+          </div>
+        </div>
+        <div className="qrcode">
+          <QRCodeSVG
+            className="img"
+            value={props["course_qr_code_url"]}
+            size={280}
+            bgColor="transparent"
+            fgColor="#333"
+          />
+        </div>
+        <div className="desc">{props["course_id"]}</div>
+      </div>
+    </div>
+  );
+};
+export default QRCard;
