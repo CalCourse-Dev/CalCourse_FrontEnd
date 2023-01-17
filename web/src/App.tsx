8a7122c06820d67df5555b0a920ee54e61ad532c
akmazian <113816245+akmazian@users.noreply.github.com>
2023-01-16 20:22:50 -0800

    App: Added testing routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,33 +1,39 @@
-import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import "./App.css";
+import React from "react"
+import { Routes, Route, BrowserRouter, Outlet } from "react-router-dom"
+import "./App.css"
 
-import Login from "./components/User/Login/Login";
-import UserPortal from "./components/User/UserPortal/UserPortal";
-import CodingLounge from "./components/CodingLounge/CodingLounge";
-import Dashboard from "./components/Dashboard/Dashboard";
-import AcademicPanel from "./components/AcademicPanel/AcademicPanel";
-import EventOverview from "./components/Event/EventOverview";
-import RequestPage from "./components/Dashboard/RequestPage/RequestPage";
-import Test from "./test";
-import 'antd/dist/antd.css';
+import Login from "./components/User/Login/Login"
+import UserPortal from "./components/User/UserPortal/UserPortal"
+import CodingLounge from "./components/CodingLounge/CodingLounge"
+import Dashboard from "./components/Dashboard/Dashboard"
+import AcademicPanel from "./components/AcademicPanel/AcademicPanel"
+import EventOverview from "./components/Event/EventOverview"
+import RequestPage from "./components/Dashboard/RequestPage/RequestPage"
+import Test from "./test"
+import "antd/dist/antd.css"
+import TestingDashboard from "./components/AZ's Dashboard/TestingDashboard.page"
 function App() {
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/coding-lounge" element={<CodingLounge />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/dashboard/request" element={<RequestPage />}/>
-        <Route path="/academic-panel" element={<AcademicPanel />} />
-        <Route path="/userportal" element={<UserPortal />} />
-        <Route path="/event-overview" element={<EventOverview />} />
-        <Route path="/testing" element={<Test />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/coding-lounge" element={<CodingLounge />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/dashboard/request" element={<RequestPage />} />
+                <Route path="/academic-panel" element={<AcademicPanel />} />
+                <Route path="/userportal" element={<UserPortal />} />
+                <Route path="/event-overview" element={<EventOverview />} />
+                <Route path="/testing" element={<Test />} />
+                <Route path="/AZ" element={<Outlet />}>
+                    <Route
+                        path="/AZ/dashboard"
+                        element={<TestingDashboard />}
+                    />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
