df7890ea00e6c89bdf114a7e1b97c3ed363aa955
akmazian <113816245+akmazian@users.noreply.github.com>
2023-01-16 20:25:23 -0800

    TestingDashboard.page: Updated filter

--- web/src/components/AZ's Dashboard/TestingDashboard.page.tsx
+++ web/src/components/AZ's Dashboard/TestingDashboard.page.tsx
@@ -2,14 +2,12 @@ import { ChangeEvent, useEffect, useState } from "react"
 import CourseAPI from "../../requests/CourseAPI"
 import type { CourseData } from "../../utils/interfaces"
 import styled from "styled-components"
+import { Input } from "antd"
 
 import "./Dashboard.scss"
-import { Input } from "antd"
+
 import QRCard from "./QRCard/QRCard"
-
-import { CONSTANTS } from "../../utils/constants"
-
-const BASE_URL = CONSTANTS.AWS_API_BASE_URL
+import UtilCard from "./Card/UtilCard"
 
 const TestingDashboard = () => {
     const [courses, set_courses] = useState<Array<CourseData>>([])
@@ -22,7 +20,20 @@ const TestingDashboard = () => {
     >([])
 
     // @audit HC
-    const [term, set_term] = useState("UCB Sp23")
+    const [selected_term, set_selected_term] = useState("UCB Sp23")
+
+    const terms = [
+        { school_name_and_term: "UCB Sp23", label: "Spring 2023 课群" },
+        { school_name_and_term: "UCB Fa22", label: "Fall 2022 课群" },
+        { school_name_and_term: "UCB Mj01", label: "专业群" },
+        { school_name_and_term: "UCB Lf01", label: "Cal Life" },
+    ]
+
+    const util_cards = [
+        { icon: "📃", label: "申请建群" },
+        { icon: "⬆️", label: "故障报告" },
+        { icon: "🔒", label: "退出登陆" },
+    ]
 
     useEffect(() => {
         const getCourses = async () => {
@@ -47,22 +58,24 @@ const TestingDashboard = () => {
             courses.filter(course => {
                 return course["school_name_and_term"]
                     .toLowerCase()
-                    .includes(term.toLowerCase())
+                    .includes(selected_term.toLowerCase())
             })
         )
-    }, [courses, term])
+    }, [courses, selected_term])
 
     useEffect(() => {
         console.log(courses_this_term)
         set_displayed_courses(
-            courses_this_term.filter(course => {
-                return (
-                    course["course_name"]
-                        .toLowerCase()
-                        .includes(search_string) ||
-                    course["course_id"].toString().includes(search_string)
-                )
-            })
+            courses_this_term
+                .filter(course => {
+                    return (
+                        course["course_name"]
+                            .toLowerCase()
+                            .includes(search_string) ||
+                        course["course_id"].toString().includes(search_string)
+                    )
+                })
+                .splice(0, 11)
         )
     }, [courses_this_term, search_string])
 
@@ -70,7 +83,7 @@ const TestingDashboard = () => {
 
     return (
         <div id="main">
-            <h1 id="title">Cal Course</h1>
+            <Title>Cal Course</Title>
             <Input
                 id="searchBar"
                 placeholder="搜索课号"
@@ -79,17 +92,61 @@ const TestingDashboard = () => {
                     set_search_string(event.target.value.toLowerCase())
                 }}
             />
-            <div id="filterBar"></div>
+            <div id="filterBar">
+                {terms.map(term => (
+                    <TermButton
+                        key={term["school_name_and_term"]}
+                        selected={
+                            term["school_name_and_term"] === selected_term
+                        }
+                        onClick={() =>
+                            set_selected_term(term["school_name_and_term"])
+                        }
+                    >
+                        {term["label"]}
+                    </TermButton>
+                ))}
+            </div>
 
             <div id="main-container">
-                {displayed_courses.map(course => {
-                    return QRCard(course)
-                })}
+                {displayed_courses.map(course => QRCard(course))}
 
                 {/* @audit add functional cards */}
+
+                {/* {util_cards.map(card => UtilCard(card))} */}
             </div>
         </div>
     )
 }
 
 export default TestingDashboard
+
+const TermButton = styled.button<{ selected: boolean }>`
+    width: 160px;
+    min-width: 140px;
+    background-color: ${props =>
+        props.selected ? "var(--accent) " : "var(--p-bg)"};
+    transition: background-color 0.2s;
+    border: 2px solid var(--accent);
+    color: ${props => (props.selected ? "var(--p-fg) " : "var(--accent)")};
+    padding: 4px;
+    border-radius: 16px;
+    margin-right: 16px;
+    box-sizing: border-box;
+    text-align: center;
+    cursor: pointer;
+`
+
+const Title = styled.h1`
+    font-weight: 800;
+    width: fit-content;
+    cursor: pointer;
+    color: white;
+    text-decoration: none;
+    font-size: 400%;
+    margin: 0 auto 0;
+    padding-top: 40px;
+    user-select: none;
+    -webkit-user-select: none;
+    text-align: left;
+`
