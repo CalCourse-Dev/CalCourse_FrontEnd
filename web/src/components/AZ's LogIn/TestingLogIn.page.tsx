bcfe1f3f3ab27465c403174bac82d5b5b6a68237
akmazian <113816245+akmazian@users.noreply.github.com>
2023-01-16 20:26:23 -0800

    TestingLogin.page: Created; unfinished

---
+++ web/src/components/AZ's LogIn/TestingLogIn.page.tsx
@@ -0,0 +1,20 @@
+//  Sign Up Page for Dashboard
+//      by AZ
+
+//  TBC:
+//      [] Component
+//      [] Cognito
+//      [] autocomplete
+//      [] onEnter
+
+
+
+const TestingLogIn = () => {
+    return (
+        <div>
+            <div></div>
+        </div>
+    )
+}
+
+export default TestingLogIn
