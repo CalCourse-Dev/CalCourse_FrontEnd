96139578fe6f0b71722215a7e174b6cff7f33bc9
akmazian <113816245+akmazian@users.noreply.github.com>
2023-02-18 23:16:45 -0800

    Dashboard Search Optimization (#14)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,33 +1,32 @@
-import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import "./App.css";
+import React from "react"
+import { Routes, Route, BrowserRouter } from "react-router-dom"
 
-import Login from "./components/User/Login/Login";
-import UserPortal from "./components/User/UserPortal/UserPortal";
-import CodingLounge from "./components/CodingLounge/CodingLounge";
-import Dashboard from "./components/Dashboard/Dashboard";
-import AcademicPanel from "./components/AcademicPanel/AcademicPanel";
-import EventOverview from "./components/Event/EventOverview";
-import RequestPage from "./components/Dashboard/RequestPage/RequestPage";
-import Test from "./test";
-import 'antd/dist/antd.css';
+import Login from "./components/User/Login/Login"
+import UserPortal from "./components/User/UserPortal/UserPortal"
+import CodingLounge from "./components/CodingLounge/CodingLounge"
+import Dashboard from "./components/Dashboard/Dashboard"
+import AcademicPanel from "./components/AcademicPanel/AcademicPanel"
+import EventOverview from "./components/Event/EventOverview"
+import RequestPage from "./components/Dashboard/RequestPage/RequestPage"
+import Test from "./test"
+import "antd/dist/antd.css"
+
 function App() {
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/coding-lounge" element={<CodingLounge />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/dashboard/request" element={<RequestPage />}/>
-        <Route path="/academic-panel" element={<AcademicPanel />} />
-        <Route path="/userportal" element={<UserPortal />} />
-        <Route path="/event-overview" element={<EventOverview />} />
-        <Route path="/testing" element={<Test />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/coding-lounge" element={<CodingLounge />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/dashboard/request" element={<RequestPage />} />
+                <Route path="/academic-panel" element={<AcademicPanel />} />
+                <Route path="/userportal" element={<UserPortal />} />
+                <Route path="/event-overview" element={<EventOverview />} />
+                <Route path="/testing" element={<Test />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
---
+++ web/src/utils/az's-util.ts
@@ -0,0 +1,13 @@
+export const parse_search_string = (search_string: string): string => {
+    // if (search_string.length === 5) {
+    //     return search_string as number as string
+    // }
+
+    if (search_string.includes("MCB")) {
+        
+    }
+
+
+
+    return search_string
+}
