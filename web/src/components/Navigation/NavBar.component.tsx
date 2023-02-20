3230ae9bcbad155cd9cf5b0a053c84a1e74e0664
akmazian <azz@berkeley.edu>
2023-02-19 19:08:08 -0800

    Navigation: styling

--- web/src/components/Navigation/NavBar.component.tsx
+++ web/src/components/Navigation/NavBar.component.tsx
@@ -1,32 +1,36 @@
+import './NavBar.css'
+import { IoBookmark, IoSearchCircle, IoBook } from 'react-icons/io5'
+import type { IconType } from 'react-icons'
+
 const navbar_items: INavBarItem[] = [
     {
         label: 'Hub',
-        icon: 'Hub',
+        icon: IoBookmark,
     },
     {
         label: '群聊查找',
-        icon: 'groupchats',
+        icon: IoSearchCircle,
         link: 'dashboard',
     },
     {
         label: '学术资源',
-        icon: 'academic-resouces',
+        icon: IoBook,
     },
     {
         label: 'FAQ & 帮助',
-        icon: 'faq',
+        icon: IoBook,
     },
 ]
 
 interface INavBarItem {
     label: string
-    icon: string
+    icon: IconType
     link?: string
 }
 
 const NavBar = () => {
     return (
-        <nav className="fixed bg-slate-500 w-[20vw] min-h-screen min-w-[250px] p-4">
+        <nav className="navbar-inner-shadow backdrop-blur fixed w-[20vw] min-h-screen min-w-[250px] p-4 border-2 border-[#A0A0A0] border-opacity-40 z-50">
             <h1 className="mx-auto w-full text-4xl mt-10 text-center font-logo font-black text-[#030944] opacity-[.87]">
                 CalCourse
             </h1>
@@ -41,8 +45,9 @@ export default NavBar
 
 const NavBarItem = (item: INavBarItem) => {
     return (
-        <li className="right-0">
-            <h2 className="">{item.label}</h2>
+        <li className="right-0 h-12 grid grid-cols-[1fr_2rem] gap-4 items-center" key={item.label}>
+            <h2 className="text-xl text-right leading-none m-0">{item.label}</h2>
+            {<item.icon className='h-6 w-6' />}
         </li>
     )
 }
--- web/src/components/Navigation/Navigation.structure.tsx
+++ web/src/components/Navigation/Navigation.structure.tsx
@@ -1,14 +1,16 @@
 import { Fragment } from 'react'
 import { Outlet } from 'react-router-dom'
+import Background from './Background.component'
 import NavBar from './NavBar.component'
 
 const Navigation = () => {
     return (
         <div className='min-h-min'>
             <NavBar />
-            <main className="ml-[20vw]">
+            <main className="ml-[20vw] z-10">
                 <Outlet />
             </main>
+            <Background />
         </div>
     )
 }
