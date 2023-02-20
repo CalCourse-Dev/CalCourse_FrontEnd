61d9d8b65e09f41053c4f5bd1759ae2fd88c8b65
akmazian <azz@berkeley.edu>
2023-02-19 19:07:53 -0800

    Bg.component: created

---
+++ web/src/components/Navigation/Background.component.tsx
@@ -0,0 +1,9 @@
+const Background = () => {
+    return (
+        <div className="gradient-background fixed top-0 block w-screen h-screen z-[-999]">
+            <div className="w-full"></div>
+        </div>
+    )
+}
+
+export default Background
