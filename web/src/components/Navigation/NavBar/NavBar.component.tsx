395a86f4c3bd4a7220011deb2228f2416fd316f3
akmazian <azz@berkeley.edu>
2023-02-25 00:35:28 -0800

    Navigation: fixed width & spacing

--- web/src/components/Navigation/NavBar/NavBar.component.tsx
+++ web/src/components/Navigation/NavBar/NavBar.component.tsx
@@ -5,11 +5,11 @@ import Footer from './Footer.component'
 
 const NavBar = () => {
     return (
-        <nav className="navbar-inner-shadow backdrop-blur fixed w-[20vw] min-h-screen min-w-[250px] p-4 border-2 border-[#A0A0A0] border-opacity-40 z-50">
-            <h1 className="mx-auto w-full text-4xl mt-10 text-center font-logo font-black text-logo opacity-[.87]">
+        <nav className="navbar-inner-shadow backdrop-blur fixed w-[min(20vw,300px)] min-h-screen min-w-[250px] border-2 border-[#A0A0A0] border-opacity-40 z-50">
+            <h1 className="mx-auto w-full text-4xl mt-12 text-center font-logo font-black text-logo opacity-[.87]">
                 CalCourse
             </h1>
-            <ul className="right-0 mt-36">
+            <ul className="right-0 mt-36 pr-4">
                 {navbar_items.map(item => NavBarItem(item))}
             </ul>
             <Footer />
