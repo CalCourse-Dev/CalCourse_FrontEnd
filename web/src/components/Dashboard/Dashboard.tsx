51a5174f6d62c35d32b2b1797adee63b2fb1017c
akmazian <azz@berkeley.edu>
2023-05-24 16:35:02 -0700

    Dashboard: enhanced search bar

--- web/src/components/Dashboard/Dashboard.tsx
+++ web/src/components/Dashboard/Dashboard.tsx
@@ -15,6 +15,8 @@ const Dashboard = () => {
         Array<CourseData>
     >([])
 
+    const [search_subject, set_search_subject] = useState('')
+
     // * Processes search string
     // * abbr => full course name
     //   e.g. 'cs' => 'compsci'
@@ -44,6 +46,14 @@ const Dashboard = () => {
             default:
                 return search_string.toLowerCase()
         }
+
+        var returned_string = search_string.toLowerCase()
+
+        for (const key in replacement_dict) {
+            returned_string.replace(key, replacement_dict[key])
+        }
+
+        return returned_string
     }
 
     const terms: ITerm[] = [
@@ -100,16 +110,21 @@ const Dashboard = () => {
         set_displayed_courses(
             courses_this_term
                 .filter(course => {
+                    const match_string =
+                        (search_subject ? search_subject + ' ' : '') +
+                        search_string
                     return (
                         course.course_name
                             .toLowerCase()
-                            .includes(parse_search_string(search_string)) ||
-                        course.course_id.toString().includes(search_string)
+                            .includes(parse_search_string(match_string)) ||
+                        course.course_id.toString().includes(match_string)
                     )
                 })
                 .sort((course1, course2) => {
                     return (
-                        parseInt((course1.course_name.match(/\d+/) ?? ['0'])[0]) -
+                        parseInt(
+                            (course1.course_name.match(/\d+/) ?? ['0'])[0]
+                        ) -
                         parseInt((course2.course_name.match(/\d+/) ?? ['0'])[0])
                     )
                 })
@@ -120,14 +135,40 @@ const Dashboard = () => {
     return (
         <Fragment>
             {/* Search Bar */}
-            <input
-                id="searchBar"
-                className="outline-0 flex pt-32 mb-[20px] mx-auto w-[800px] text-xl pl-2 relative text-graphite bg-transparent bg-[#00000000] border-solid border-b-2 border-b-[#555] hover:border-b-[var(--accent)] focus:border-solid focus:border-b-2 focus:border-b-[var(--accent)]"
-                placeholder="搜索课号"
-                onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                    set_search_string(event.target.value.toLowerCase())
-                }}
-            />
+            <div id="search-bar-container" className="mt-32 mb-[20px] mx-auto">
+                <input
+                    id="search-bar-subject"
+                    className={`outline-0 ${
+                        search_subject === '' ? 'hidden' : 'inline-block'
+                    } w-auto text-xl pl-2 relative text-graphite bg-transparent bg-[#00000000] border-solid border-b-2 border-b-[#555] hover:border-b-[var(--accent)] focus:border-solid focus:border-b-2 focus:border-b-[var(--accent)]`}
+                    placeholder={search_subject}
+                    onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                        set_search_subject(event.target.value.toUpperCase())
+                    }}
+                    value={search_subject}
+                    onKeyUp={event => {}}
+                />
+                <input
+                    id="searchBar"
+                    className="outline-0 inline-block w-auto text-xl pl-2 relative text-graphite bg-transparent bg-[#00000000] border-solid border-b-2 border-b-[#555] hover:border-b-[var(--accent)] focus:border-solid focus:border-b-2 focus:border-b-[var(--accent)]"
+                    placeholder="搜索课号"
+                    onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                        set_search_string(event.target.value.toLowerCase())
+                    }}
+                    value={search_string}
+                    onKeyUp={event => {
+                        if (event.key === 'Enter') {
+                            if (
+                                search_string ===
+                                (search_string.match(/[a-zA-Z]+/) ?? [''])[0]
+                            ) {
+                                set_search_subject(search_string.toUpperCase())
+                                set_search_string('')
+                            }
+                        }
+                    }}
+                />
+            </div>
             {/* Terms / Categories Bar */}
             <div
                 id="filterBar"
