a0968638e5f89e817381032251e2f9c5b92909ef
akmazian <azz@berkeley.edu>
2023-05-29 17:31:11 -0700

    CourseCard: remove unused components

--- web/src/components/Dashboard/CourseCard/CourseCard.component.tsx
+++ web/src/components/Dashboard/CourseCard/CourseCard.component.tsx
@@ -2,6 +2,8 @@ import { QRCodeSVG } from 'qrcode.react'
 import { Component } from 'react'
 
 import type { CourseData } from '../../../utils/interfaces'
+import { Transition } from '@headlessui/react'
+import { ActiveCardsContext } from '../../../contexts/ActiveCards.context'
 
 interface PCourseCard {
     course: CourseData
@@ -13,38 +15,69 @@ interface SCourseCard {
 
 class CourseCard extends Component<PCourseCard, SCourseCard> {
     state: SCourseCard = {
-        showing_details: false,
+        showing_details: false
     }
 
     render() {
+        const { course_name, course_id, course_qr_code_url } = this.props.course
+
         return (
             <div
-                className={`card-transluscent h-64 w-full duration-300 overflow-hidden`}
+                className={`${
+                    this.state.showing_details
+                        ? 'card-transluscent-active'
+                        : 'card-transluscent'
+                } h-64 w-full duration-300 overflow-hidden flex justify-center items-center flex-col`}
                 onClick={() => {
                     this.setState(state => ({
-                        showing_details: !state.showing_details,
+                        showing_details: !state.showing_details
                     }))
                 }}
             >
-                <h1 className="text-subtitle text-center align-middle text-lg h-min mt-24">
-                    {this.props.course.course_name}
-                </h1>
-                <div
-                    className={`relative ${
-                        this.state.showing_details ? 'top-[100]' : ''
-                    } duration-200`}
+                <Transition
+                    show={this.state.showing_details}
+                    enter="transition-transform duration-250"
+                    enterFrom="-translate-y-full"
+                    enterTo="translate-0"
+                    leave="transition-transform duration-250"
+                    leaveFrom="translate-0"
+                    leaveTo="-translate-y-full"
+                    className="top-0 relative mx-auto"
                 >
-                    {/* <QRCodeSVG
-                        className="img"
-                        value={this.props.course.course_qr_code_url}
-                        size={280}
+                    <QRCodeSVG
+                        className="mx-auto text-logo mt-4"
+                        value={course_qr_code_url}
+                        size={160}
                         bgColor="transparent"
-                        fgColor="#333"
-                    /> */}
-                </div>
-                <div className="bg-accent w-full h-6 absolute bottom-0 text-center opacity-stroke text-white font-bold align-middle">
-                    {this.props.course.course_id}
-                </div>
+                        fgColor="#212121"
+                    />
+                </Transition>
+
+                <h1 className="block text-center text-lg h-min mx-auto">
+                    {course_name}
+                </h1>
+
+                {/* <Transition
+                    show={!this.state.showing_details}
+                    enter="transition-transform duration-150"
+                    enterFrom="translate-y-full"
+                    enterTo="translate-y-0"
+                    leave="transition-transform duration-150"
+                    leaveFrom="translate-y-0"
+                    leaveTo="translate-y-full"
+                    className="bg-accent w-full h-6 absolute bottom-0 text-center opacity-stroke text-white font-bold align-middle"
+                >
+                    {course_id}
+                </Transition> */}
+                <span
+                    className={`bg-accent w-full h-6 absolute bottom-0 text-center ${
+                        this.state.showing_details
+                            ? 'opacity-1'
+                            : 'opacity-stroke'
+                    } transition-opacity duration-150 text-white font-bold align-middle`}
+                >
+                    {course_id}
+                </span>
             </div>
         )
     }
