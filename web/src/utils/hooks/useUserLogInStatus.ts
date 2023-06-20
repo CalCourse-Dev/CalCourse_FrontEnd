2f6857ab185ab8d864ac75eac1d59ccfae53b9d2
akmazian <azz@berkeley.edu>
2023-06-19 19:59:25 -0500

    useUserLogInStatus: reverted to prev ver

--- web/src/utils/hooks/useUserLogInStatus.ts
+++ web/src/utils/hooks/useUserLogInStatus.ts
@@ -4,8 +4,5 @@ import { UserContext } from '../../contexts/User.context'
 export const useUserLogInStatus = (): boolean => {
     const { user } = useContext(UserContext)
 
-    // return user !== null
-
-    // hard-coded for testing, uncomment the line above when login is finished
-    return true
+    return user !== null
 }
