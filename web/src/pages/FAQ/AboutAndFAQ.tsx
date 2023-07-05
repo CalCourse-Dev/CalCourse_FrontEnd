a78b58e120842be1798e5eb8b7cfb7e354e63e38
akmazian <azz@berkeley.edu>
2023-07-05 15:52:41 -0700

    AboutAndFAQ: cleaned up

--- web/src/pages/FAQ/AboutAndFAQ.tsx
+++ web/src/pages/FAQ/AboutAndFAQ.tsx
@@ -5,15 +5,20 @@ const AboutAndFAQ = () => {
         <div>
             <h1>About Us</h1>
             <h1>Frequently Asked Questions</h1>
-            <FAQ />
+            <FAQs />
         </div>
     )
 }
 
-const questions: { question: string; Answer: FC<{ className: string }> }[] = [
+interface IFAQ {
+    question: string
+    Answer: FC<{ className: string }>
+}
+
+const questions: IFAQ[] = [
     {
         question: "What's your cookies policy?",
-        Answer: ({ className }: { className: string }) => (
+        Answer: ({ className }) => (
             <p className={className}>
                 As of Jun. 20, the rendition currently published is not using
                 Cookies. We have migrated all of our Cookies-based
@@ -24,8 +29,8 @@ const questions: { question: string; Answer: FC<{ className: string }> }[] = [
     },
     {
         question: 'What about data security?',
-        Answer: () => (
-            <p>
+        Answer: ({ className }) => (
+            <p className={className}>
                 All of our data is currently housed on AWS (that's Amazon Web
                 Services if you are wondering, it's also the largest cloud
                 computing and storage service on our Earth), so everything
@@ -36,35 +41,17 @@ const questions: { question: string; Answer: FC<{ className: string }> }[] = [
     }
 ]
 
-const FAQ = () => {
+const FAQs = () => {
+    return <div>{questions.map(faq => FAQ(faq))}</div>
+}
+
+const FAQ = ({ question, Answer }: IFAQ) => {
     return (
-        <div>
-            {questions.map(({ question, Answer }) => {
-                return (
-                    <div className="">
-                        <h2 className="">{question}</h2>
-                        <Answer className="" />
-                    </div>
-                )
-            })}
+        <div className="">
+            <h2 className="">{question}</h2>
+            <Answer className="" />
         </div>
     )
 }
-
-const CookiesPolicy = () => {
-    return (
-        <div id="cookies-policy-container">
-            <h1>Our Cookies Policies</h1>
-            <p>
-                As of Jun. 20, the rendition currently published is not using
-                Cookies. We have migrated all of our Cookies-based
-                functionalities to either sessionStorage, localStorage, or our
-                secure AWS server.
-            </p>
-        </div>
-    )
-}
-
-const DataSecurity = () => {}
 
 export default AboutAndFAQ
