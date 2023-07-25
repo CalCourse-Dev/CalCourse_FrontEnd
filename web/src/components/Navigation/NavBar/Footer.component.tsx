3b1b8309ab3dc2bcc43d66a525e5102467283ffe
akmazian <azz@berkeley.edu>
2023-07-24 21:18:13 -0700

    Navigation: added responsive support

--- web/src/components/Navigation/NavBar/Footer.component.tsx
+++ web/src/components/Navigation/NavBar/Footer.component.tsx
@@ -1,13 +1,15 @@
 const Footer = () => {
     return (
-        <footer className="w-full text-center text-sm mb-4 px-4 text-graphite opacity-60 mobile:hidden">
-            Learn more about the team{' '}
-            <a
-                href="/faq#team"
-                className="underline decoration-1 decoration-dotted underline-offset-2"
-            >
-                here
-            </a>
+        <footer className="w-full text-center text-sm mb-4 px-4 text-graphite opacity-60 h-10">
+            <div className="sm:hidden">
+                Learn more about the team{' '}
+                <a
+                    href="/faq#team"
+                    className="underline decoration-1 decoration-dotted underline-offset-2"
+                >
+                    here
+                </a>
+            </div>
         </footer>
     )
 }
