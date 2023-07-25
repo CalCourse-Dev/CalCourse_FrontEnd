b6cd90bba06fc9e9162b39f22bdc754ded9a1ce0
akmazian <azz@berkeley.edu>
2023-07-25 04:08:02 -0700

    Hub: removed unused imports

--- web/src/pages/Hub/Hub.tsx
+++ web/src/pages/Hub/Hub.tsx
@@ -1,11 +1,8 @@
-import React, { useState } from "react";
-import PanelCard from "./PanelCard/PanelCard";
-
 const Hub = () => {
-  return (
-    <div>
-      <h1>FIXME</h1>
-    </div>
-  );
-};
-export default Hub;
+    return (
+        <div>
+            <h1>FIXME</h1>
+        </div>
+    )
+}
+export default Hub
