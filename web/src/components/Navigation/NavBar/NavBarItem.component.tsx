c44487a2b025f5f5c0fcad25f691a2ec00d29972
akmazian <azz@berkeley.edu>
2023-07-25 04:07:20 -0700

    NavBarItem: responsive & dark mode support

--- web/src/components/Navigation/NavBar/NavBarItem.component.tsx
+++ web/src/components/Navigation/NavBar/NavBarItem.component.tsx
@@ -16,9 +16,11 @@ const NavBarItem = (item: INavBarItem) => {
     return (
         // the interpolated tenary operator changes opacity
         <li
-            className={`my-auto group h-12 text-graphite flex gap-4 flex-row-reverse sm:flex-row mb:justify-center mb:items-center duration-150 ${
-                user_logged_in ? 'cursor-pointer' : 'cursor-default'
-            }  ${selected ? 'opacity-100' : 'opacity-30 hover:opacity-70'}`}
+            className={`my-auto h-12 flex gap-4 flex-row-reverse sm:flex-row mb:justify-center mb:items-center duration-150 ${
+                user_logged_in ? 'cursor-pointer group' : 'cursor-default'
+            }  ${selected ? 'opacity-100' : 'opacity-30'} ${
+                !selected && user_logged_in && 'hover:opacity-70'
+            }`}
             key={item.label}
             onClick={() => {
                 if (user_logged_in) {
@@ -26,7 +28,7 @@ const NavBarItem = (item: INavBarItem) => {
                 }
             }}
         >
-            <h2 className="text-xl text-right leading-none m-0 font-bold mb:hidden sm:hidden mb:group-hover:block sm:group-hover:block mb:fixed sm:fixed mb:mb-[7.5rem] sm:ml-14 mb:card-transluscent sm:card-transluscent mb:p-2 sm:p-2 rounded-2xl w-max order-2">
+            <h2 className="text-xl text-right leading-none m-0 font-bold mb:hidden sm:hidden mb:group-hover:block sm:group-hover:block mb:fixed sm:fixed mb:mb-[7.5rem] sm:ml-14 mb:card-transluscent sm:card-transluscent mb:py-2 sm:py-2 mb:px-3 sm:px-3 mb:font-medium sm:font-medium rounded-2xl w-max order-2">
                 {item.label}
             </h2>
             {/* the icon is a react-icon object */}
