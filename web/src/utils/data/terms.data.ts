308e70d4763ba15bab5acaba632c0134b3866e0f
akmazian <azz@berkeley.edu>
2023-08-01 20:17:18 -0700

    terms: rearranged

--- web/src/utils/data/terms.data.ts
+++ web/src/utils/data/terms.data.ts
@@ -1,8 +1,8 @@
 import type { ITerm } from "../interfaces/interfaces";
 
 export const TERMS: ITerm[] = [
+    { school_name_and_term: 'UCB Mj01', label: '专业群' },
     { school_name_and_term: 'UCB Su23', label: 'Summer 2023 课群' },
     { school_name_and_term: 'UCB Fa23', label: 'Fall 2023 课群' },
-    { school_name_and_term: 'UCB Mj01', label: '专业群' },
     { school_name_and_term: 'UCB Lf01', label: 'Cal Life' }
 ]
