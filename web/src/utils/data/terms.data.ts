b9964fc1fc5cb1e38ee2540c707cf27cbadf875e
Huanzhi Mao <huanzhimao@gmail.com>
2023-09-23 17:02:55 -0700

    temp fix back

--- web/src/utils/data/terms.data.ts
+++ web/src/utils/data/terms.data.ts
@@ -1,8 +1,8 @@
 import type { ITerm } from '../interfaces/interfaces'
 
 export const TERMS: ITerm[] = [
-    // { school_name_and_term: 'UCB Mj01', label: '专业群' },
-    { school_name_and_term: 'UCLA Fall', label: 'Fall 课群' },
+    { school_name_and_term: 'UCB Mj01', label: '专业群' },
+    { school_name_and_term: 'UCB Fa23', label: 'Fall 课群' },
     // {
     //     school_name_and_term: 'UCB Sp24',
     //     label: 'Spring 2024 课群',
@@ -14,5 +14,5 @@ export const TERMS: ITerm[] = [
     //     label: 'Spring 2025 课群',
     //     hidden: true
     // },
-    // { school_name_and_term: 'UCB Lf01', label: 'Cal Life' }
+    { school_name_and_term: 'UCB Lf01', label: 'Cal Life' }
 ]
